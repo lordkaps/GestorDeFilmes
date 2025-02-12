e7db9e9999a700b0afe87015229d8ec776e5cdf8
lordkaps <augustokaps@gmail.com>
2025-02-12 17:51:06 -0300

    Adicionar arquivos de projeto.

---
+++ GestorDeFilmes/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GestorDeFilmes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GestorDeFilmes/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GestorDeFilmes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
